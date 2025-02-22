e3430d5e008fde629a64924c4d19462701fa4e00
Alessandro Perna <lorenzo1956lorenzo@gmail.com>
2025-02-22 09:14:16 +0100

    Commit 2025-02-22 09:14:05

--- src/Animale.java
+++ src/Animale.java
@@ -1,5 +1,13 @@
 public abstract class Animale {
-    private String nome;
-    private int age;
+    protected final String nome;
+    protected int age;
+    protected final int LIFE_SPAN;
+
+    public Animale(int LIFE_SPAN, String nome) {
+        this.LIFE_SPAN = LIFE_SPAN;
+        this.nome = nome;
+    }
+
     public abstract void reproduce();
+    public abstract void eat();
 }
--- src/Lupo.java
+++ src/Lupo.java
@@ -1,1 +1,16 @@
+public class Lupo extends Animale {
+    public Lupo(String nome) {
+        super(15, nome);
 
+    }
+
+
+
+    public void reproduce() {
+        //TODO
+    }
+
+    public void eat() {
+        //TODO
+    }
+}
