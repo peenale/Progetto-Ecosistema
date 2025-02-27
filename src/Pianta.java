b6bf82ae37a3d98bbb2ee84aa03948874eacb64c
livia_kaq0no <liviabonetti2@gmail.com>
2025-02-27 11:40:19 +0100

    Aggiunte funzionalità alla classe Pianta

--- src/Pianta.java
+++ src/Pianta.java
@@ -1,2 +1,28 @@
-package PACKAGE_NAME;public class Pianta {
+public class Pianta {
+    private SpeciePianta speciePianta;
+    private int eta;
+    private boolean adulta, frutti;
+
+    public Pianta(SpeciePianta speciePianta, int eta, boolean adulta, boolean frutti){
+        this.speciePianta = speciePianta;
+        eta = 0;
+        adulta = false;
+        frutti = false;
+    }
+
+    public int getEta() {
+        return eta;
+    }
+
+    public void cresce(){
+        eta++;
+        if(eta>=speciePianta.getGiorniCrescita())
+            adulta = true;
+        if(eta>=speciePianta.getGiorniCrescita())
+            frutti = true;
+    }
+
+    public boolean muore(){
+        return eta>=speciePianta.getTempoVita();
+    }
 }
