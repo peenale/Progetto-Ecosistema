21d9068b161f9fefb7cdb87fa6c2bcb7c7df5681
Alessandro Perna <lorenzo1956lorenzo@gmail.com>
2025-03-08 08:31:31 +0100

    Creazoine main

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,25 @@
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        List<Animale> animali_iniziali = List.of(
+                new Animale(SpecieAnimalie.PINGUINO_TATTICO_NUCLEARE_IMPERATORE, 0, 0, 'm', "Gaio Giulio Cesare Ottaviano Augusto"),
+                new Animale(SpecieAnimalie.PINGUINO_TATTICO_NUCLEARE_IMPERATORE, 0, 0, 'f', "Augusto Ottaviano Cesare Giulio Gaio"),
+                new Animale(SpecieAnimalie.ORNITORINCO, 0, 0, 'm', "Perry"),
+                new Animale(SpecieAnimalie.ORNITORINCO, 0, 0, 'f', "Reppy"),
+                new Animale(SpecieAnimalie.DODO, 0, 0, 'm', "🤡"),
+                new Animale(SpecieAnimalie.DODO, 0, 0, 'f', "🍋")
+        );
+        List<Pianta> piante_iniziali = List.of(
+                new Pianta(SpeciePianta.ALLORO),
+                new Pianta(SpeciePianta.CANAPA),
+                new Pianta(SpeciePianta.ACACIA)
+        );
+
+        Ecosistema ecosistema = new Ecosistema(animali_iniziali, piante_iniziali);
+
+        while (!ecosistema.getAnimali().isEmpty()) {
+            ecosistema.avanzaGiorno();
+        }
     }
 }
